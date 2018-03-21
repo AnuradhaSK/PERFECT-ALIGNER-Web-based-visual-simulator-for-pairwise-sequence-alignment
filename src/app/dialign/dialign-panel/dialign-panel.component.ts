a29a9ecb0db2327d3d99cfdc65b38545935bf25c
unknown <anuradha199528@gmail.com>
2018-03-21 16:56:41 +0530

    pannels modified

--- src/app/dialign/dialign-panel/dialign-panel.component.ts
+++ src/app/dialign/dialign-panel/dialign-panel.component.ts
@@ -11,12 +11,10 @@ import {Gene} from '../../gene';
   styleUrls: ['./dialign-panel.component.css']
 })
 
-export class DialignPanelComponent implements OnInit, AfterViewInit {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+export class DialignPanelComponent implements OnInit {
   geneArray: any;
-  displayedColumns = ['id', 'sequence'];
-  dataSource: any;
-
+  maxlength: number;
+  width: any;
 
   constructor(protected dialignService: DialignService,
               protected msasharingService: MsaSharingService) {
@@ -24,59 +22,26 @@ export class DialignPanelComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.geneArray = this.msasharingService.getPanelData();
-    this.dataSource = this.geneArray;
   }
 
-  /**
-   * Set the paginator after the view init since this component will
-   * be able to query its view for the initialized paginator.
-   */
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
 
   getMaxSequenceLength() {
-    let maxlength = 0;
+    this.maxlength = 0;
     let max = [];
     for (let i in this.geneArray) {
-      if (maxlength < this.geneArray[i].sequence.split('').length) {
-        maxlength = this.geneArray[i].sequence.split('').length;
+      if (this.maxlength < this.geneArray[i].sequence.split('').length) {
+        this.maxlength = this.geneArray[i].sequence.split('').length;
         max = this.geneArray[i].sequence.split('');
       }
     }
-    console.log(maxlength);
+    console.log(this.maxlength);
+    this.calWidth();
     return max;
 
   }
-}
 
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  calWidth() {
+    this.width = 100 + 30 * this.maxlength;
+  }
 }
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
 
